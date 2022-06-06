3b24200c9f36953800c6f2b524de02133b0d385a
Adrian Liechti <adrian@monobox.ch>
2022-06-06 12:37:39 +0200

    implement inspect wrapper

--- pkg/container/engine/engine.go
+++ pkg/container/engine/engine.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"context"
 	"io"
+	"net"
 )
 
 type PullOptions struct {
@@ -27,5 +28,54 @@ type Engine interface {
 
 	Remove(ctx context.Context, container string, options RemoveOptions) error
 
+	Inspect(ctx context.Context, container string) (Container, error)
+
 	Logs(ctx context.Context, container string, options LogsOptions) error
+}
+
+type Container struct {
+	ID   string
+	Name string
+
+	Labels map[string]string
+
+	Image string
+
+	Privileged bool
+	RunAsUser  string
+	RunAsGroup string
+
+	Env map[string]string
+	Dir string
+
+	Command []string
+	Args    []string
+
+	Hostname  string
+	IPAddress net.IP
+
+	Ports  []ContainerPort
+	Mounts []ContainerMount
+}
+
+type Protocol string
+
+const (
+	ProtocolTCP Protocol = "tcp"
+	ProtocolUDP Protocol = "udp"
+)
+
+type ContainerPort struct {
+	Port  int
+	Proto Protocol
+
+	HostIP   string
+	HostPort *int
+}
+
+type ContainerMount struct {
+	Path string
+
+	Volume   string
+	HostPath string
 }
--- pkg/container/moby/moby.go
+++ pkg/container/moby/moby.go
@@ -5,6 +5,8 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"io"
+	"net"
+	"strconv"
 	"strings"
 
 	"github.com/adrianliechti/devkit/pkg/container/engine"
@@ -71,6 +73,16 @@ func (m *Moby) Remove(ctx context.Context, container string, options engine.Remo
 	})
 }
 
+func (m *Moby) Inspect(ctx context.Context, container string) (engine.Container, error) {
+	data, err := m.client.ContainerInspect(ctx, container)
+
+	if err != nil {
+		return engine.Container{}, err
+	}
+
+	return convertContainer(data), nil
+}
+
 func (m *Moby) Logs(ctx context.Context, container string, options engine.LogsOptions) error {
 	if options.Stdout == nil {
 		options.Stdout = io.Discard
@@ -131,4 +143,86 @@ func registryCredentials(image string) string {
 	}
 
 	return base64.URLEncoding.EncodeToString(data)
+}
+
+func convertContainer(data types.ContainerJSON) engine.Container {
+	container := engine.Container{
+		ID:   data.ID,
+		Name: strings.TrimLeft(data.Name, "/"),
+
+		Labels: data.Config.Labels,
+
+		Image: data.Config.Image,
+
+		Privileged: data.HostConfig.Privileged,
+
+		Env: map[string]string{},
+		Dir: data.Config.WorkingDir,
+
+		Command: data.Config.Entrypoint,
+		Args:    data.Config.Cmd,
+
+		Hostname:  data.Config.Hostname,
+		IPAddress: net.ParseIP(data.NetworkSettings.IPAddress),
+
+		Ports:  []engine.ContainerPort{},
+		Mounts: []engine.ContainerMount{},
+	}
+
+	if data.Config.User != "" {
+		s := strings.Split(data.Config.User, ":")
+
+		if len(s) > 0 {
+			container.RunAsUser = s[0]
+		}
+
+		if len(s) > 1 {
+			container.RunAsGroup = s[1]
+		}
+	}
+
+	for _, e := range data.Config.Env {
+		s := strings.SplitN(e, "=", 2)
+
+		key := s[0]
+		val := s[1]
+
+		container.Env[key] = val
+	}
+
+	for p, m := range data.NetworkSettings.Ports {
+		for _, b := range m {
+			port := engine.ContainerPort{
+				Port:  p.Int(),
+				Proto: engine.Protocol(p.Proto()),
+			}
+
+			if b.HostPort != "" {
+				if val, err := strconv.Atoi(b.HostPort); err == nil {
+					port.HostIP = b.HostIP
+					port.HostPort = &val
+				}
+			}
+
+			container.Ports = append(container.Ports, port)
+		}
+	}
+
+	for _, m := range data.Mounts {
+		mount := engine.ContainerMount{
+			Path: m.Destination,
+		}
+
+		if m.Type == "bind" {
+			mount.HostPath = m.Source
+		}
+
+		if m.Type == "volume" {
+			mount.Volume = m.Name
+		}
+
+		container.Mounts = append(container.Mounts, mount)
+	}
+
+	return container
 }
