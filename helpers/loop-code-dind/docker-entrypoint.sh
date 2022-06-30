83a16b6e08fa008222713382b53b08e8ae9f395f
Adrian Liechti <adrian.liechti@zuehlke.com>
2022-06-30 12:59:33 +0200

    move code & jenkins to separate repo

--- helpers/loop-code-dind/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo -nb /usr/local/bin/dind /usr/local/bin/dockerd --group code --iptables=false
-
-exec "$@"
--- helpers/loop-jenkins-dind/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo -nb /usr/local/bin/dind /usr/local/bin/dockerd --group jenkins --iptables=false
-
-exec "$@"
