81042a7e3fd5491ce52bee03eeaa838930ab7db7
Adrian Liechti <adrian@monobox.ch>
2022-10-02 14:00:21 +0200

    add cockroachdb (#5)
    Co-authored-by: Adrian Liechti <adrian.liechti@zuehlke.com>

---
+++ pkg/catalog/cockroachdb/cockroachdb.go
@@ -0,0 +1,87 @@
+package cockroachdb
+
+import (
+	"github.com/adrianliechti/devkit/pkg/catalog"
+	"github.com/adrianliechti/devkit/pkg/engine"
+)
+
+var (
+	_ catalog.Manager         = &Manager{}
+	_ catalog.Decorator       = &Manager{}
+	_ catalog.ShellProvider   = &Manager{}
+	_ catalog.ConsoleProvider = &Manager{}
+)
+
+type Manager struct {
+}
+
+func (m *Manager) Name() string {
+	return "cockroachdb"
+}
+
+func (m *Manager) Category() catalog.Category {
+	return catalog.DatabaseCategory
+}
+
+func (m *Manager) DisplayName() string {
+	return "CockroachDB"
+}
+
+func (m *Manager) Description() string {
+	return "A distributed SQL datatabase designed for speed, scale, and survival."
+}
+
+const (
+	DefaultShell = "/bin/bash"
+)
+
+func (m *Manager) New() (engine.Container, error) {
+	image := "cockroachdb/cockroach:v22.1.8"
+
+	return engine.Container{
+		Image: image,
+
+		Env: map[string]string{},
+
+		Ports: []*engine.ContainerPort{
+			{
+				Port:  26257,
+				Proto: engine.ProtocolTCP,
+			},
+		},
+
+		Args: []string{
+			"start-single-node",
+			"--insecure",
+		},
+
+		Mounts: []*engine.ContainerMount{
+			{
+				Path: "/cockroach/cockroach-data",
+			},
+		},
+	}, nil
+}
+
+func (m *Manager) Info(instance engine.Container) (map[string]string, error) {
+	return map[string]string{}, nil
+}
+
+func (m *Manager) Shell(instance engine.Container) (string, error) {
+	return DefaultShell, nil
+}
+
+func (m *Manager) Client(instance engine.Container) (string, []string, error) {
+	return "", []string{
+		DefaultShell,
+		"-c",
+		"./cockroach sql --insecure",
+	}, nil
+}
+
+func (m *Manager) ConsolePort(instance engine.Container) (*engine.ContainerPort, error) {
+	return &engine.ContainerPort{
+		Port:  8080,
+		Proto: engine.ProtocolTCP,
+	}, nil
+}
