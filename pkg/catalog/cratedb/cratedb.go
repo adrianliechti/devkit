9d69cba7f99c8ee5a0bb6de2400d8e2de8bae446
Adrian Liechti <adrian.liechti@zuehlke.com>
2022-12-10 11:58:17 +0100

    cratedb: arm64 + cli

--- pkg/catalog/cratedb/cratedb.go
+++ pkg/catalog/cratedb/cratedb.go
@@ -1,6 +1,8 @@
 package cratedb
 
 import (
+	"runtime"
+
 	"github.com/adrianliechti/devkit/pkg/catalog"
 	"github.com/adrianliechti/devkit/pkg/engine"
 )
@@ -9,6 +11,7 @@ var (
 	_ catalog.Manager         = &Manager{}
 	_ catalog.Decorator       = &Manager{}
 	_ catalog.ShellProvider   = &Manager{}
+	_ catalog.ClientProvider  = &Manager{}
 	_ catalog.ConsoleProvider = &Manager{}
 )
 
@@ -37,6 +40,10 @@ const (
 
 func (m *Manager) New() (engine.Container, error) {
 	image := "crate/crate:latest"
+
+	if runtime.GOARCH == "arm64" {
+		image = "arm64v8/crate:latest"
+	}
 
 	return engine.Container{
 		Image: image,
@@ -69,6 +76,14 @@ func (m *Manager) Info(instance engine.Container) (map[string]string, error) {
 
 func (m *Manager) Shell(instance engine.Container) (string, error) {
 	return DefaultShell, nil
+}
+
+func (m *Manager) Client(instance engine.Container) (string, []string, error) {
+	return "", []string{
+		DefaultShell,
+		"-c",
+		"su - crate -c 'crash --hosts localhost'",
+	}, nil
 }
 
 func (m *Manager) ConsolePort(instance engine.Container) (*engine.ContainerPort, error) {
